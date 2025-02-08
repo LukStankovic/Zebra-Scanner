ae086bda6a09a1b1deb2d30d965295cb53d3e06e
Lukáš Stankovič <lukasstankovic@gmail.com>
2025-02-08 20:45:51 +0100

    Init

---
+++ app/src/main/java/com/stankovic/zebrascanner/config/ScanningConfig.kt
@@ -0,0 +1,6 @@
+package com.stankovic.zebrascanner.config
+
+object ScanningConfig {
+    const val APP_SCANNER_INTENT = "com.stankovic.zebrascanner.scan"
+    const val ZEBRA_SCANNER_INTENT = "com.symbol.datawedge.data_string"
+}
