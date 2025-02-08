d1d9012923c5916cbe8755172e3dcc4b573e154f
Lukáš Stankovič <lukasstankovic@gmail.com>
2025-02-08 21:19:40 +0100

    Add scanning

--- app/src/main/java/com/stankovic/zebrascanner/MainActivity.kt
+++ app/src/main/java/com/stankovic/zebrascanner/MainActivity.kt
@@ -1,47 +1,59 @@
 package com.stankovic.zebrascanner
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.stankovic.zebrascanner.config.ScanningConfig
+import com.stankovic.zebrascanner.scanning.ScanBroadcastReceiver
+import com.stankovic.zebrascanner.ui.screen.ScanningScreen
 import com.stankovic.zebrascanner.ui.theme.ZebraScannerTheme
+import com.stankovic.zebrascanner.viewmodel.ScannerViewModel
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+    private val scannerViewModel: ScannerViewModel by viewModels()
+    private lateinit var scannerReceiver: ScanBroadcastReceiver
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        scannerReceiver = ScanBroadcastReceiver(
+            scannerViewModel,
+        )
+        registerScannerReceiver()
+
         setContent {
             ZebraScannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScanningScreen(
+                    scannerViewModel = scannerViewModel,
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun registerScannerReceiver() {
+        val filter = IntentFilter()
+        filter.addCategory(Intent.CATEGORY_DEFAULT)
+        filter.addAction(ScanningConfig.APP_SCANNER_INTENT)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZebraScannerTheme {
-        Greeting("Android")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(scannerReceiver, filter, RECEIVER_EXPORTED)
+        } else {
+            @SuppressLint("UnspecifiedRegisterReceiverFlag")
+            registerReceiver(scannerReceiver, filter)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        registerScannerReceiver()
     }
 }
