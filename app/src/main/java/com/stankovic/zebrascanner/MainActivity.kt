231c43d0ff3b7bf5d789b0b1706c0d8f07e3ba41
Lukas Stankovic <lukas.stankovic@footshop.cz>
2025-06-13 21:45:13 +0200

    Create fixes for Android 13
    - Add Hilt for dependency injection by setting up the Application class and annotating MainActivity.
    - Update AGP to 8.10.1.
    - Update various library versions including Kotlin, coreKtx, lifecycleRuntimeKtx, composeBom, Hilt, and KSP.
    - Remove `androidx.hilt.work` dependency.
    - Refactor ScanBroadcastReceiver registration/unregistration in MainActivity to prevent multiple registrations and ensure proper unregistration during onPause.

--- app/src/main/java/com/stankovic/zebrascanner/MainActivity.kt
+++ app/src/main/java/com/stankovic/zebrascanner/MainActivity.kt
@@ -14,20 +14,18 @@ import com.stankovic.zebrascanner.scanning.ScanBroadcastReceiver
 import com.stankovic.zebrascanner.ui.screen.ScanningScreen
 import com.stankovic.zebrascanner.ui.theme.ZebraScannerTheme
 import com.stankovic.zebrascanner.viewmodel.ScannerViewModel
-import kotlin.getValue
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val scannerViewModel: ScannerViewModel by viewModels()
     private lateinit var scannerReceiver: ScanBroadcastReceiver
-
+    private var isReceiverRegistered = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        scannerReceiver = ScanBroadcastReceiver(
-            scannerViewModel,
-        )
-        registerScannerReceiver()
+        scannerReceiver = ScanBroadcastReceiver(scannerViewModel)
 
         setContent {
             ZebraScannerTheme {
@@ -39,21 +37,35 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun registerScannerReceiver() {
-        val filter = IntentFilter()
-        filter.addCategory(Intent.CATEGORY_DEFAULT)
-        filter.addAction(ScanningConfig.APP_SCANNER_INTENT)
+        if (!isReceiverRegistered) {
+            val filter = IntentFilter()
+            filter.addCategory(Intent.CATEGORY_DEFAULT)
+            filter.addAction(ScanningConfig.APP_SCANNER_INTENT)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            registerReceiver(scannerReceiver, filter, RECEIVER_EXPORTED)
-        } else {
-            @SuppressLint("UnspecifiedRegisterReceiverFlag")
-            registerReceiver(scannerReceiver, filter)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                registerReceiver(scannerReceiver, filter, RECEIVER_EXPORTED)
+            } else {
+                @SuppressLint("UnspecifiedRegisterReceiverFlag")
+                registerReceiver(scannerReceiver, filter)
+            }
+            isReceiverRegistered = true
+        }
+    }
+
+    private fun unregisterScannerReceiver() {
+        if (isReceiverRegistered) {
+            unregisterReceiver(scannerReceiver)
+            isReceiverRegistered = false
         }
     }
 
     override fun onResume() {
         super.onResume()
-
         registerScannerReceiver()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterScannerReceiver()
     }
 }
